aa4ca76bae399b19e807c30b48ad2f3d459f3b62
Harikrishnan <harikrishkk@gmail.com>
2021-07-08 19:53:08 -0400

    Add props to user component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import User from '@components/User';
 import Navbar from '@components/Navbar';
 
-function App() {
+const user = {
+  id: 1,
+  email: 'user@test.com',
+  first_name: 'John',
+  last_name: 'Doe',
+  avatar: 'https://reqres.in/img/faces/1-image.jpg',
+};
+const App = () => {
   return (
-    // Static card html
-    <div className="h-screen p-6 bg-gray-200">
+    <div className="h-full p-6 bg-gray-200">
       <Navbar />
-      <User />
+      <User user={user} />
     </div>
   );
-}
+};
 
 export default App;
