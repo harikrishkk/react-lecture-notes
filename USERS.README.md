9d1ce108dcdae0b02c47921efa506d3dc03a54ef
Harikrishnan <harikrishkk@gmail.com>
2021-07-11 14:16:47 -0400

    Add axios to explore useEffect

---
+++ USERS.README.md
@@ -0,0 +1,47 @@
+{
+"id": 1,
+"email": "george.bluth@test.com",
+"first_name": "George",
+"last_name": "Bluth",
+"avatar": "https://reqres.in/img/faces/1-image.jpg",
+"rating": 4,
+"hobbies": [
+"Music",
+"Photography"
+]
+}
+{
+"id": 2,
+"email": "janet.weaver@test.com",
+"first_name": "Janet",
+"last_name": "Weaver",
+"avatar": "https://reqres.in/img/faces/2-image.jpg",
+"rating": 3,
+"hobbies": [
+"Dance",
+"Programming"
+]
+}
+{
+"id": 3,
+"email": "emma.wong@test.com",
+"first_name": "Emma",
+"last_name": "Wong",
+"avatar": "https://reqres.in/img/faces/3-image.jpg",
+"rating": 5,
+"hobbies": [
+"Cooking",
+"Marathon"
+]
+}
+{
+"id": 4,
+"email": "eve.holt@test.com",
+"first_name": "Eve",
+"last_name": "Holt",
+"avatar": "https://reqres.in/img/faces/4-image.jpg",
+"rating": 2,
+"hobbies": [
+"Gardening"
+]
+}
--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import User from '@components/User';
 import Navbar from '@components/Navbar';
-import { users } from './users.data';
 import Notification from '@components/Notification';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import UserForm from '@components/UserForm';
 import Newsletter from '@components/Newsletter';
+import axios from 'axios';
+
 const App = () => {
-  const [userData, setUserData] = useState(users);
+  const [userData, setUserData] = useState([]);
   const [currentUser, setCurrentUser] = useState();
+  const [counter, setCounter] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    console.log(`${counter}: Featching data from Backend..`);
+    axios
+      .get('https://react-notes-2673f-default-rtdb.firebaseio.com/users.json')
+      .then((data) => {
+        const responseArr = [];
+        Object.keys(data.data).forEach((item) => {
+          responseArr.push({
+            ...data.data[item],
+            id: item,
+          });
+        });
+        setUserData(responseArr);
+      });
+    return () => {
+      // cleanup.
+      console.log('Will run once before the next time useEffect runs again.');
+    };
+  }, [counter]);
+
+  const increment = () => {
+    setCounter((c) => c + 1);
+  };
+
   const handleUserSelect = (user) => {
     setCurrentUser(user);
   };
@@ -31,11 +59,21 @@ const App = () => {
         />
       )}
 
-      <div className="grid grid-cols-2 gap-4">
-        {userData.map((user) => (
-          <User onUserSelect={handleUserSelect} key={user.id} user={user} />
-        ))}
+      <div className="py-4 flex flex-row-reverse">
+        <button onClick={increment} className="btn btn-sm">
+          Increment
+        </button>
+        <button onClick={() => setVisible((v) => !v)} className="btn btn-sm">
+          Toggle Users
+        </button>
       </div>
+      {visible && (
+        <div className="grid grid-cols-2 gap-4">
+          {userData.map((user) => (
+            <User onUserSelect={handleUserSelect} key={user.id} user={user} />
+          ))}
+        </div>
+      )}
 
       <UserForm onUserAdd={handleUserAdd} />
       <Newsletter />
